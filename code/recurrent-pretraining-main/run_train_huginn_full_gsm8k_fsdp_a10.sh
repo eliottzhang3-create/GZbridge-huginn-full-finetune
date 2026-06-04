d96f8ecc6af9e04b02ce1851870542144e3d6500
Jinwei Zhang <flyfireworks331@sjtu.edu.cn>
2026-06-04 22:07:20 +0800

    Add A10-specific finetuning and launch scripts

---
+++ code/recurrent-pretraining-main/run_train_huginn_full_gsm8k_fsdp_a10.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p log
+
+vc submit \
+  -p A10_PARTITION_NAME_HERE \
+  -i docker.v2.aispeech.com/sjtu/sjtu_wumengyue-mhl:0.0.1 \
+  -c 32 -m 320G -g 8 \
+  -n 1 \
+  -j huginn-full-gsm8k-fsdp-a10-$(date +%m%d%H%M) \
+  -d "$(pwd)" \
+  JOB=1:1 "$(pwd)/log/huginn_full_gsm8k_fsdp_a10.JOB.log" \
+  --cmd "bash local_scripts/train_huginn_full_gsm8k_fsdp_a10.sh"
