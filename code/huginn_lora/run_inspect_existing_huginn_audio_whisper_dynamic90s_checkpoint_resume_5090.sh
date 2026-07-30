c9633a1925046c9d779339f45065ab5c6ee0bd07
Jinwei Zhang <flyfireworks331@sjtu.edu.cn>
2026-07-30 11:58:33 +0800

    +

--- code/huginn_lora/run_inspect_existing_huginn_audio_whisper_dynamic90s_checkpoint_resume_5090.sh
+++ code/huginn_lora/run_inspect_existing_huginn_audio_whisper_dynamic90s_checkpoint_resume_5090.sh
@@ -15,10 +15,9 @@ printf -v quoted_run_root '%q' "$RUN_ROOT"
 vc submit \
   -p pdgpu-5090 \
   -i docker.v2.aispeech.com/sjtu/sjtu_wumengyue-mhl:0.0.1 \
-  -c 8 -m 64G -g 1 \
+  -c 8 -m 32G -g 1 \
   -n 1 \
   -j inspect-whisper-dyn90-existing-ckpt-$(date +%m%d%H%M) \
   -d "$SCRIPT_DIR" \
   JOB=1:1 "$SCRIPT_DIR/log/inspect_existing_huginn_audio_whisper_dynamic90s_checkpoint_resume_5090.JOB.log" \
   --cmd "HUGINN_AUDIO_DYNAMIC90S_EXISTING_CHECKPOINT_RUN_ROOT=${quoted_run_root} bash scripts/inspect_existing_huginn_audio_whisper_dynamic90s_checkpoint_resume.sh"
-
